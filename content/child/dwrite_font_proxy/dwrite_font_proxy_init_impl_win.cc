e969d6ffdc6815827297612b76ba547fdce6cdd1
Shane Fournier <93808768+win32ss@users.noreply.github.com>
2023-06-28 13:54:09 -0400

    Revert "Delete unused font fallback code"
    This reverts commit 469877fbaf6409f8a444c26612e519e80a76e6c8.
    
    Reverting removal of font fallback code.

--- content/child/dwrite_font_proxy/dwrite_font_proxy_init_impl_win.cc
+++ content/child/dwrite_font_proxy/dwrite_font_proxy_init_impl_win.cc
@@ -78,16 +78,8 @@ void InitializeDWriteFontProxy() {
   blink::WebFontRendering::SetFontRenderingClient(g_font_collection);
 
   skia::OverrideDefaultSkFontMgr(std::move(skia_font_manager));
-  // When IDWriteFontFallback is not available (prior to Win8.1) Skia will
-  // still attempt to use DirectWrite to determine fallback fonts (in
-  // SkFontMgr_DirectWrite::onMatchFamilyStyleCharacter), which will likely
-  // result in trying to load the system font collection. To avoid that and
-  // instead fall back on WebKit's fallback logic, we don't use Skia's font
-  // fallback if IDWriteFontFallback is not available.
-  bool fallback_available = g_font_fallback != nullptr;
-  DCHECK_EQ(fallback_available,
-            base::win::GetVersion() > base::win::Version::WIN8);
-  blink::WebFontRendering::SetUseSkiaFontFallback(fallback_available);
+
+  blink::WebFontRendering::SetUseSkiaFontFallback(g_font_fallback ? true : false);
 }
 
 void UninitializeDWriteFontProxy() {
