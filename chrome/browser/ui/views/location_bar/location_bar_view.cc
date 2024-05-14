b84830e922855ce459f110febe5a9ceaa7788c71
win32ss <win32ss@nulln.nl>
2024-05-14 12:50:23 -0400

    (#572) classic-omnibox-border on its own will now draw the special address bar centre border as well.

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -1171,7 +1171,8 @@ void LocationBarView::RefreshBackground() {
   // Keep the views::Textfield in sync. It needs an opaque background to
   // correctly enable subpixel AA.
   omnibox_view_->SetBackgroundColor(background_color);
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch("classic-omnibox") &&
+  if ((base::CommandLine::ForCurrentProcess()->HasSwitch("classic-omnibox") ||
+      base::CommandLine::ForCurrentProcess()->HasSwitch("classic-omnibox-border"))&&
       base::CommandLine::ForCurrentProcess()->HasSwitch("compact-ui"))
         // When the location bar is shrunken, the border above is only drawn on the sides.
         // To resolve this, an extra border is drawn in that area on the top and bottom.
